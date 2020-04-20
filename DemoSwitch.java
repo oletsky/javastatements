0d38839819e4386818914fc50abc91959e2e96ca
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-20 14:41:57 +0300

    Add files via upload

---
+++ DemoSwitch.java
@@ -0,0 +1,22 @@
+package javatraining.oletsky.statements;
+
+/**
+ * Illustrating switch
+ */
+public class DemoSwitch {
+    public static void main(String[] args) {
+        int a = 2;
+        switch (a) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+        }
+
+    }
+}
