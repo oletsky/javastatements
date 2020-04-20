d8ab0ce507d126dbcc79c5960eaeb9542135dd4b
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-20 14:17:57 +0300

    Add files via upload

---
+++ DemoWhile.java
@@ -0,0 +1,17 @@
+package javatraining.oletsky.statements;
+
+/**
+ * Illustrating while loop
+ */
+public class DemoWhile {
+    public static void main(String[] args) {
+        int n = 10;
+        int i=1;
+        while (i<=n) {
+            System.out.println(i);
+            i++;
+        }
+
+    }
+
+}
