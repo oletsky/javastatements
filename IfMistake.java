d85251da1865de13caf7f05618881e9392a3397c
Oleksiy Oletsky <oletsky@ukr.net>
2020-04-20 14:14:30 +0300

    Add files via upload

---
+++ IfMistake.java
@@ -0,0 +1,13 @@
+package javatraining.oletsky.statements;
+
+/**
+ * What is wrong with this comparison? Try to fix it
+ */
+public class IfMistake {
+    public static void main(String[] args) {
+        int a=1000;
+        if (a==1000) System.out.println("Equals 1000");
+        System.out.println("Not equals 1000");
+    }
+
+}
